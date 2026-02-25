9baedf1ba501cdba07900907054df4854b426ef6
Will  <speedbolt1004@gmail.com>
2026-02-25 15:13:25 +0700

    coprpashmu done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   useEffect(() => {
     initGrid();
     // initAscii();
-    
+
     const links = document.querySelectorAll(".frame__links a");
     const bg = document.querySelector(".frame__links .link-bg");
 
@@ -42,7 +42,7 @@ export default function App() {
           this piece is best viewed<br />
           on desktop screen.
         </p>
-        
+
         <h1 className="mobile__title">(wob)</h1>
         <div className="mobile__desc2">
           <a href="https://linkedin.com/in/owil/" target="_blank" rel="noreferrer">{"corporate"}</a>
@@ -50,13 +50,13 @@ export default function App() {
           <a href="mailto:owilhm@gmail.com" target="_blank" rel="noreferrer">{"hmu"}</a>
         </div>
       </div>
-      
+
       <div className="loading-overlay" id="loading-overlay">
         <div className="loading-overlay__content">
           <div className="loading-overlay__progress">0</div>
         </div>
       </div>
-      
+
       <div className="overlay" id="overlay"></div>
 
       <header className="frame">
@@ -90,7 +90,7 @@ export default function App() {
               <p>static</p>
               <div className="hud2__radar">
                 <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M22.25 0.25L22.25 44.25M44.25 21.25L0.25 21.25M0.250009 0.25H44.25V44.25H0.250009V0.25Z" stroke="#1A1A1A" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3"/>
+                  <path d="M22.25 0.25L22.25 44.25M44.25 21.25L0.25 21.25M0.250009 0.25H44.25V44.25H0.250009V0.25Z" stroke="#1A1A1A" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
                 </svg>
 
                 {/* <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -194,7 +194,7 @@ export default function App() {
           <p data-title="4" data-text>kix</p>
           <p data-title="5" data-text>mytelkomsel basic</p>
           <p data-title="6" data-text>samosynth</p>
-          <p data-title="7" data-text>owillhem</p>
+          <p data-title="7" data-text>obed willhem</p>
           <p data-title="8" data-text>thanh uy art gallery</p>
           <p data-title="9" data-text>viettel</p>
           <p data-title="10" data-text>blooms</p>
@@ -210,7 +210,7 @@ export default function App() {
                 <p>📍 indonesia</p>
                 <p>🕰️ 2024</p>
               </span>
-              A self-care application for AXIS customers that allows them to manage their accounts, top up credits & internet packages. 
+              A self-care application for AXIS customers that allows them to manage their accounts, top up credits & internet packages.
               <span>T B U</span>
             </p>
 
@@ -219,7 +219,7 @@ export default function App() {
                 <p>📍 vietnam</p>
                 <p>🕰️ 2025</p>
               </span>
-              A digital B2B banking service offered by the Vietnam Joint Stock Commercial Bank for Industry and Trade. Allows corporate clients to manage accounts, conduct transactions, and approve payments anytime and anywhere via an internet connection. 
+              A digital B2B banking service offered by the Vietnam Joint Stock Commercial Bank for Industry and Trade. Allows corporate clients to manage accounts, conduct transactions, and approve payments anytime and anywhere via an internet connection.
               <span>T B U</span>
             </p>
 
@@ -228,7 +228,7 @@ export default function App() {
                 <p>📍 indonesia</p>
                 <p>🕰️ 2025</p>
               </span>
-              A recent GoPay digital wallet service integration for Telkomsel users via the MyTelkomsel app, offering convenience and efficiency in transacting Telkomsel services. 
+              A recent GoPay digital wallet service integration for Telkomsel users via the MyTelkomsel app, offering convenience and efficiency in transacting Telkomsel services.
               <span>T B U</span>
             </p>
             <p data-desc="4" data-text>
@@ -248,7 +248,7 @@ export default function App() {
                 <p>📍 indonesia</p>
                 <p>🕰️ 2024</p>
               </span>
-              A lightweight application from Telkomsel, designed for users with limited memory or in areas with unstable internet connections. Provides essential functions such as credit & data balance monitoring, internet & phone package shop, and making payments. 
+              A lightweight application from Telkomsel, designed for users with limited memory or in areas with unstable internet connections. Provides essential functions such as credit & data balance monitoring, internet & phone package shop, and making payments.
               <span>T B U</span>
             </p>
 
@@ -259,11 +259,11 @@ export default function App() {
                 <p>🕰️ 2025</p>
               </span>
               <span>
-                <img className="details__media" src="./ssamo_dsimulator.png"/>
+                <img className="details__media" src="./ssamo_dsimulator.png" />
               </span>
               <span>
                 <p>
-                samosynth is a mini mpc / sampler that celebrates the bataknese culture, by pouring relevant bataknese elements and including easter-eggs into the music experience. the drum kit contains a mix of hip-hop drum samples + processed bataknese traditional instruments (taganing, kulcapi and gordang) one-shots.
+                  samosynth is a mini mpc / sampler that celebrates the bataknese culture, by pouring relevant bataknese elements and including easter-eggs into the music experience. the drum kit contains a mix of hip-hop drum samples + processed bataknese traditional instruments (taganing, kulcapi and gordang) one-shots.
                 </p>
               </span>
               <span>pad mechanism</span>
@@ -272,13 +272,13 @@ export default function App() {
               </span>
               <span>
                 <p>
-                i wanted to introduce bataknese music & visuals holistically in a pleasurable manner. 
-                <br />
-                <br />this mechanism was inspired by the gorogoa game, which i adore a lot. the individual drumpads is a puzzle piece, that when rotated in a certain angle forms a combination that will unlock a new drum presets & patterns inspired from some key bataknese cultural elements. 
-                <br />
-                <br />each preset represents a region from north sumatera region, with the sound and pattern is inspired from those region.
-                <br />
-                <br />you can play with the drumpads as it is, or unlock other drum presets by completing the puzzle on the drumpads. 
+                  i wanted to introduce bataknese music & visuals holistically in a pleasurable manner.
+                  <br />
+                  <br />this mechanism was inspired by the gorogoa game, which i adore a lot. the individual drumpads is a puzzle piece, that when rotated in a certain angle forms a combination that will unlock a new drum presets & patterns inspired from some key bataknese cultural elements.
+                  <br />
+                  <br />each preset represents a region from north sumatera region, with the sound and pattern is inspired from those region.
+                  <br />
+                  <br />you can play with the drumpads as it is, or unlock other drum presets by completing the puzzle on the drumpads.
                 </p>
               </span>
               <span>tiles</span>
@@ -287,11 +287,11 @@ export default function App() {
               </span>
               <span>
                 <p>
-                  i spent a good amount of time exploring and experimenting on different tile patterns and grid sizes. 
+                  i spent a good amount of time exploring and experimenting on different tile patterns and grid sizes.
                 </p>
               </span>
               <span>
-                <img className="details__media" src="./ssamo_dgrid.png"/>
+                <img className="details__media" src="./ssamo_dgrid.png" />
               </span>
             </p>
 
@@ -326,7 +326,7 @@ export default function App() {
                 <p>FKJ</p>
                 <p>deGoogling</p>
               </span>
-              <span>
+              <span style={{ display: "flex", gap: "10px" }}>
                 <a href="https://linkedin.com/in/owil/" target="_blank" rel="noreferrer">{"corporate"}</a>
                 <a href="https://drive.google.com/file/d/1wnpzDhdp48MtSadvQc9Sbi5i7vKP3uiw/view?usp=sharing" target="_blank" rel="noreferrer">{"pause"}</a>
                 <a href="mailto:owilhm@gmail.com" target="_blank" rel="noreferrer">{"hmu"}</a>
@@ -358,7 +358,7 @@ export default function App() {
                 <p>📍 australia</p>
                 <p>🕰️ 2025</p>
               </span>
-              An Australian retail pharmacy group with a network of independently owned and operated pharmacies, also includes a certified B Corp management services arm. The pharmacies offer health & wellness prescription, pharmacist consultations, and medication ordering. 
+              An Australian retail pharmacy group with a network of independently owned and operated pharmacies, also includes a certified B Corp management services arm. The pharmacies offer health & wellness prescription, pharmacist consultations, and medication ordering.
               <br />
               <span>
                 <a href="https://joinbloomsthechemist.com.au/" target="_blank" rel="noreferrer">{"live site here"}</a>
@@ -380,7 +380,7 @@ export default function App() {
                 <p>📍 indonesia</p>
                 <p>🕰️ 2024</p>
               </span>
-              The B2B unit of Telkomsel that provides digital solutions and connectivity for corporations to support their digital transformation. Offering advanced network services, communication & collaboration tools, IoT solutions, and CX insights tools. 
+              The B2B unit of Telkomsel that provides digital solutions and connectivity for corporations to support their digital transformation. Offering advanced network services, communication & collaboration tools, IoT solutions, and CX insights tools.
               <br />
               <span>T B U</span>
             </p>
@@ -398,14 +398,14 @@ export default function App() {
         </svg> */}
         <svg width="52" height="52" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
           {/* <path d="M0 2.13709L0.432567 1.36051L2.9244 2.87755L2.85129 0H3.71642L3.64331 2.87153L6.13514 1.36051L6.56771 2.13709L4.02714 3.50363L6.56771 4.87016L6.13514 5.64674L3.64331 4.13572L3.71642 7.01327H2.85129L2.9244 4.13572L0.432567 5.64674L0 4.87016L2.53448 3.50363L0 2.13709Z" fill="#161616"/> */}
-          <path d="M9.35807 11.3838L9.79064 10.6072L12.2825 12.1242L12.2094 9.24668H13.0745L13.0014 12.1182L15.4932 10.6072L15.9258 11.3838L13.3852 12.7503L15.9258 14.1168L15.4932 14.8934L13.0014 13.3824L13.0745 16.26H12.2094L12.2825 13.3824L9.79064 14.8934L9.35807 14.1168L11.8926 12.7503L9.35807 11.3838Z" fill="#161616"/>
-          <path d="M18.7161 20.6305L19.1487 19.8539L21.6405 21.3709L21.5674 18.4934H22.4326L22.3595 21.3649L24.8513 19.8539L25.2839 20.6305L22.7433 21.997L25.2839 23.3635L24.8513 24.1401L22.3595 22.6291L22.4326 25.5066H21.5674L21.6405 22.6291L19.1487 24.1401L18.7161 23.3635L21.2506 21.997L18.7161 20.6305Z" fill="#161616"/>
-          <path d="M28.0742 29.8771L28.5068 29.1006L30.9986 30.6176L30.9255 27.74H31.7906L31.7175 30.6116L34.2094 29.1006L34.6419 29.8771L32.1014 31.2437L34.6419 32.6102L34.2094 33.3868L31.7175 31.8758L31.7906 34.7533H30.9255L30.9986 31.8758L28.5068 33.3868L28.0742 32.6102L30.6087 31.2437L28.0742 29.8771Z" fill="#161616"/>
+          <path d="M9.35807 11.3838L9.79064 10.6072L12.2825 12.1242L12.2094 9.24668H13.0745L13.0014 12.1182L15.4932 10.6072L15.9258 11.3838L13.3852 12.7503L15.9258 14.1168L15.4932 14.8934L13.0014 13.3824L13.0745 16.26H12.2094L12.2825 13.3824L9.79064 14.8934L9.35807 14.1168L11.8926 12.7503L9.35807 11.3838Z" fill="#161616" />
+          <path d="M18.7161 20.6305L19.1487 19.8539L21.6405 21.3709L21.5674 18.4934H22.4326L22.3595 21.3649L24.8513 19.8539L25.2839 20.6305L22.7433 21.997L25.2839 23.3635L24.8513 24.1401L22.3595 22.6291L22.4326 25.5066H21.5674L21.6405 22.6291L19.1487 24.1401L18.7161 23.3635L21.2506 21.997L18.7161 20.6305Z" fill="#161616" />
+          <path d="M28.0742 29.8771L28.5068 29.1006L30.9986 30.6176L30.9255 27.74H31.7906L31.7175 30.6116L34.2094 29.1006L34.6419 29.8771L32.1014 31.2437L34.6419 32.6102L34.2094 33.3868L31.7175 31.8758L31.7906 34.7533H30.9255L30.9986 31.8758L28.5068 33.3868L28.0742 32.6102L30.6087 31.2437L28.0742 29.8771Z" fill="#161616" />
           {/* <path d="M37.4323 39.1238L37.8649 38.3472L40.3567 39.8643L40.2836 36.9867H41.1487L41.0756 39.8583L43.5674 38.3472L44 39.1238L41.4594 40.4904L44 41.8569L43.5674 42.6335L41.0756 41.1225L41.1487 44H40.2836L40.3567 41.1225L37.8649 42.6335L37.4323 41.8569L39.9668 40.4904L37.4323 39.1238Z" fill="#161616"/> */}
           {/* <path d="M37.4323 2.13709L37.8649 1.36051L40.3567 2.87755L40.2836 0H41.1487L41.0756 2.87153L43.5674 1.36051L44 2.13709L41.4594 3.50363L44 4.87016L43.5674 5.64674L41.0756 4.13572L41.1487 7.01327H40.2836L40.3567 4.13572L37.8649 5.64674L37.4323 4.87016L39.9668 3.50363L37.4323 2.13709Z" fill="#161616"/> */}
-          <path d="M28.0742 11.3838L28.5068 10.6072L30.9986 12.1242L30.9255 9.24668H31.7906L31.7175 12.1182L34.2094 10.6072L34.6419 11.3838L32.1014 12.7503L34.6419 14.1168L34.2094 14.8934L31.7175 13.3824L31.7906 16.26H30.9255L30.9986 13.3824L28.5068 14.8934L28.0742 14.1168L30.6087 12.7503L28.0742 11.3838Z" fill="#161616"/>
+          <path d="M28.0742 11.3838L28.5068 10.6072L30.9986 12.1242L30.9255 9.24668H31.7906L31.7175 12.1182L34.2094 10.6072L34.6419 11.3838L32.1014 12.7503L34.6419 14.1168L34.2094 14.8934L31.7175 13.3824L31.7906 16.26H30.9255L30.9986 13.3824L28.5068 14.8934L28.0742 14.1168L30.6087 12.7503L28.0742 11.3838Z" fill="#161616" />
           {/* <path d="M0 39.1238L0.432567 38.3472L2.9244 39.8643L2.85129 36.9867H3.71642L3.64331 39.8583L6.13514 38.3472L6.56771 39.1238L4.02714 40.4904L6.56771 41.8569L6.13514 42.6335L3.64331 41.1225L3.71642 44H2.85129L2.9244 41.1225L0.432567 42.6335L0 41.8569L2.53448 40.4904L0 39.1238Z" fill="#161616"/> */}
-          <path d="M9.35807 29.8771L9.79064 29.1006L12.2825 30.6176L12.2094 27.74H13.0745L13.0014 30.6116L15.4932 29.1006L15.9258 29.8771L13.3852 31.2437L15.9258 32.6102L15.4932 33.3868L13.0014 31.8758L13.0745 34.7533H12.2094L12.2825 31.8758L9.79064 33.3868L9.35807 32.6102L11.8926 31.2437L9.35807 29.8771Z" fill="#161616"/>
+          <path d="M9.35807 29.8771L9.79064 29.1006L12.2825 30.6176L12.2094 27.74H13.0745L13.0014 30.6116L15.4932 29.1006L15.9258 29.8771L13.3852 31.2437L15.9258 32.6102L15.4932 33.3868L13.0014 31.8758L13.0745 34.7533H12.2094L12.2825 31.8758L9.79064 33.3868L9.35807 32.6102L11.8926 31.2437L9.35807 29.8771Z" fill="#161616" />
         </svg>
 
 
